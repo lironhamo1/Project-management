f20494aec94a1e03d2f766f029bb220b53d5157c
ohad shalom <ohad5855@gmail.com>
2021-03-13 20:59:27 +0200

    fix home page

--- src/index.js
+++ src/index.js
@@ -3,15 +3,25 @@ const express = require('express')
 // eslint-disable-next-line no-undef
 const app_port = process.env.PORT || 3000
 const app = express()
-const path = require('path')
+// eslint-disable-next-line no-undef
+var path = require('path');
 
+// eslint-disable-next-line no-undef
+const fullPath=__dirname
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,"homePage.html"))
+    res.sendFile(path.join(fullPath + '/homePage.html'))
+})
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(fullPath + '/login.html'))
+})
+app.get('/signup',(req,res)=>{
+    res.sendFile(path.join(fullPath + '/Signup.html'))
 })
 
+
 app.listen(app_port, () => {
-    console.log('app is runninng. port: ${app_port}')
-    //console.log('http://127.0.0.1:${app_port}/')
+    console.log('app is runninng. port: '+app_port)
+    console.log('http://127.0.0.1:'+app_port+"/")
 })
 
 
