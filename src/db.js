243c70098385c3ef7d3417c4cb281d116e8bf9fe
ohad shalom <ohad5855@gmail.com>
2021-04-21 15:27:37 +0300

    Database

---
+++ src/db.js
@@ -0,0 +1,19 @@
+//db.js
+
+//URL containing user and password
+const url = "mongodb+srv://our-user28:12GoTravel34@cluster0.ofal3.mongodb.net/usersDB?retryWrites=true&w=majority";
+
+var mongoose = require('mongodb').MongoClient;
+mongoose.connect(url, function(err, db) {
+    if (err) throw err;
+    //Choosing DB
+    var dbo = db.db("sample_analytics");
+
+    //Extracting data from accounts collection
+    // eslint-disable-next-line no-unused-vars
+    dbo.collection("accounts").find({}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+    });
+});
