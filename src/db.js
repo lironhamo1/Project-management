94a6c47613ea1773010bcae337054c89eb71f2cd
ohad shalom <ohad5855@gmail.com>
2021-04-26 23:33:27 +0300

    update

--- src/db.js
+++ src/db.js
@@ -4,16 +4,21 @@
 const url = "mongodb+srv://our-user28:12GoTravel34@cluster0.ofal3.mongodb.net/usersDB?retryWrites=true&w=majority";
 
 var mongoose = require('mongodb').MongoClient;
-mongoose.connect(url, function(err, db) {
-    if (err) throw err;
-    //Choosing DB
-    var dbo = db.db("sample_analytics");
 
-    //Extracting data from accounts collection
-    // eslint-disable-next-line no-unused-vars
-    dbo.collection("accounts").find({}).toArray(function(err, result) {
+
+function mongodb_extract(collection) {
+    mongoose.connect(url, function(err, db) {
         if (err) throw err;
-        console.log(result);
-        db.close();
+        //Choosing DB
+        var dbo = db.db("GoTravel");
+
+        //Extracting data from accounts collection
+        // eslint-disable-next-line no-unused-vars
+        dbo.collection(collection).find({}).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+        });
     });
-});
+}
+export {mongodb_extract}
--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ const express = require('express')
 // eslint-disable-next-line no-undef
 const app_port = process.env.PORT || 3000
 const app = express()
+app.set('view engine','ejs')
 // eslint-disable-next-line no-undef
 var path = require('path');
-
+app.set('views',path.join(__dirname,'../','views'));
+var temp;
 // eslint-disable-next-line no-undef
 const fullPath=__dirname
+let PathDi=path.join(__dirname, '../');
+const fullPathviews=path.join(PathDi,'/views');
+const fullPathimages=path.join(fullPathviews,'/images');
+const fullPathCss=path.join(PathDi,'/public/css');
+const fullPathJs=path.join(PathDi,'/public/js');
+
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(fullPath + '/homePage.html'))
 })
@@ -19,48 +28,69 @@ app.get('/signup',(req,res)=>{
     res.sendFile(path.join(fullPath + '/Signup.html'))
 })
 
-
-app.listen(app_port, () => {
-    console.log('app is runninng. port: '+app_port)
-    console.log('http://127.0.0.1:'+app_port+"/")
+app.get('/OrdersM',(req,res)=>{
+    res.render('OrdersManagementPage')
 })
-/*
-// eslint-disable-next-line no-undef
-var mongoose = require('mongoose');
-git var Schema=mongoose.Schema;
-const db = mongoose.connection;
+app.get('/logo.png',(req,res)=>{
+    res.sendFile(path.join(fullPathimages + '/logo.png'))
+})
+app.get('/p.png',(req,res)=>{
+    res.sendFile(path.join(fullPathimages + '/p.png'))
+})
+app.get('/OrdersManagementPage.css',(req,res)=>{
+    res.sendFile(path.join(fullPathCss + '/OrdersManagementPage.css'))
+})
+app.get('/OrdersManagementPage.js',(req,res)=>{
+    res.sendFile(path.join(fullPathJs + '/OrdersManagementPage.js'))
+    //res.render(path.join('/OrdersManagementPage'))
+})
 
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function() {
-    console.log("connect!!good for you!!")
-    var mys=new Schema({name:String , age:String});
-    var usermodel=mongoose.model("e",mys);
-    usermodel.find({},function(err,result){
-        console.log(result);
-    })
-});
-
-
-//mongoose.connect('mongodb://localhost:27017/e', {useNewUrlParser: true, useUnifiedTopology: true});
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-});
-
-*/
-// eslint-disable-next-line no-undef
-var mongoose = require('mongodb').MongoClient;
-mongoose.connect('mongodb://localhost:27017/', function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("mydb");
+// app.get('/readFile', async (req, res) => {
+//     //let fileContent ;
+//     const url = "mongodb+srv://our-user28:12GoTravel34@cluster0.ofal3.mongodb.net/usersDB?retryWrites=true&w=majority";
+//     var mongoose = require('mongodb').MongoClient;
+//     mongoose.connect(url, function(err, db) {
+//         if (err) throw err;
+//         //Choosing DB
+//         var dbo = db.db("GoTravel");
+//
+//         //Extracting data from accounts collection
+//         // eslint-disable-next-line no-unused-vars
+//         dbo.collection('Orders').find({}).toArray(function(err, result) {
+//             temp=true
+//             //res.render('OrdersManagementPage',{temp: true})
+//             if (err) throw err;
+//             db.close();
+//         });
+//     });
+//
+// });
 
 
-    // eslint-disable-next-line no-unused-vars
-    dbo.collection("customers").find({}).toArray(function(err, result) {
+app.post("/dani", (req, res) => {
+    const url = "mongodb+srv://our-user28:12GoTravel34@cluster0.ofal3.mongodb.net/usersDB?retryWrites=true&w=majority";
+    var mongoose = require('mongodb').MongoClient;
+    mongoose.connect(url, function (err, db) {
         if (err) throw err;
-        console.log(result);
-        db.close();
+        //Choosing DB
+        var dbo = db.db("GoTravel");
+
+        //Extracting data from accounts collection
+        // eslint-disable-next-line no-unused-vars
+        dbo.collection('Orders').find({}).toArray(function (err, result) {
+            //
+            return res.status(200).json({
+                ok: true,
+                data: result
+            });
+            if (err) throw err;
+            db.close();
+        });
     });
 });
+
+app.listen(app_port, () => {
+    console.log('app is runninng. port: '+app_port);
+    console.log('http://127.0.0.1:'+app_port+"/");
+});
+
