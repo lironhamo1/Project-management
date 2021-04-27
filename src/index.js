778f7912cde2a712f30bb0ba268b5c2801a8e24e
Einav Bitton <einav1997bi@gmail.com>
2021-04-27 22:23:46 +0300

    about us page update

--- src/index.js
+++ src/index.js
@@ -4,63 +4,124 @@ const express = require('express')
 // eslint-disable-next-line no-undef
 const app_port = process.env.PORT || 3000
 const app = express()
+app.set('view engine','ejs')
 // eslint-disable-next-line no-undef
 var path = require('path');
-
+app.set('views',path.join(__dirname,'../','views'));
+var temp;
 // eslint-disable-next-line no-undef
 const fullPath=__dirname
+let PathDi=path.join(__dirname, '../');
+const fullPathviews=path.join(PathDi,'/views');
+const fullPathimages=path.join(fullPathviews,'/images');
+const fullPathCss=path.join(PathDi,'/public/css');
+const fullPathJs=path.join(PathDi,'/public/js');
+
+
+
+app.get('/liron.png',(req,res)=>{
+    res.sendFile(path.join(fullPathimages + '/liron.png'))
+})
+app.get('/ohad.png',(req,res)=>{
+    res.sendFile(path.join(fullPathimages + '/ohad.png'))
+})
+app.get('/p_n.png',(req,res)=>{
+    res.sendFile(path.join(fullPathimages + '/p_n.png'))
+})
+app.get('/p_o.png',(req,res)=>{
+    res.sendFile(path.join(fullPathimages + '/p_o.png'))
+})
+app.get('/daniel.png',(req,res)=>{
+    res.sendFile(path.join(fullPathimages + '/daniel.png'))
+})
+app.get('/einav.png',(req,res)=>{
+    res.sendFile(path.join(fullPathimages + '/einav.png'))
+})
+app.get('/logo.png',(req,res)=>{
+    res.sendFile(path.join(fullPathimages + '/logo.png'))
+})
+app.get('/p_p.png',(req,res)=>{
+    res.sendFile(path.join(fullPathimages + '/p_p.png'))
+})
+app.get('/p.png',(req,res)=>{
+    res.sendFile(path.join(fullPathimages + '/p.png'))
+})
+
+
+/////AboutUs page
+app.get('/Aboutus.css',(req,res)=>{
+    res.sendFile(path.join(fullPathCss + '/Aboutus.css'))
+})
+app.get('/Aboutus.js',(req,res)=>{
+    res.sendFile(path.join(fullPathJs + '/Aboutus.js'))
+    //res.render(path.join('/OrdersManagementPage'))
+})
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(fullPath + '/homePage.ejs'))
-})
-app.get('/login',(req,res)=>{
-    res.sendFile(path.join(fullPath + '/login.ejs'))
-})
-app.get('/signup',(req,res)=>{
-    res.sendFile(path.join(fullPath + '/Signup.ejs'))
+    res.render( 'Aboutus')
 })
 
-
-app.listen(app_port, () => {
-    console.log('app is runninng. port: '+app_port)
-    console.log('http://127.0.0.1:'+app_port+"/")
+/////q&a page
+app.get('/q&a.css',(req,res)=>{
+    res.render(path.join(fullPathCss + '/q&a.css'))
 })
-/*
-// eslint-disable-next-line no-undef
-var mongoose = require('mongoose');
-git var Schema=mongoose.Schema;
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-db.once('open', function() {
-    console.log("connect!!good for you!!")
-    var mys=new Schema({name:String , age:String});
-    var usermodel=mongoose.model("e",mys);
-    usermodel.find({},function(err,result){
-        console.log(result);
-    })
-});
+app.get('/q&a.js',(req,res)=>{
+    res.sendFile(path.join(fullPathJs + '/q&a.js'))
+    //res.render(path.join('/OrdersManagementPage'))
+})
+app.get('/q&a.ejs',(req,res)=>{
+    res.render('q&a')
+})
 
 
-//mongoose.connect('mongodb://localhost:27017/e', {useNewUrlParser: true, useUnifiedTopology: true});
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-});
-
-*/
-// eslint-disable-next-line no-undef
-var mongoose = require('mongodb').MongoClient;
-mongoose.connect('mongodb://localhost:27017/', function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("mydb");
 
 
-    // eslint-disable-next-line no-unused-vars
-    dbo.collection("customers").find({}).toArray(function(err, result) {
+
+// app.get('/readFile', async (req, res) => {
+//     //let fileContent ;
+//     const url = "mongodb+srv://our-user28:12GoTravel34@cluster0.ofal3.mongodb.net/usersDB?retryWrites=true&w=majority";
+//     var mongoose = require('mongodb').MongoClient;
+//     mongoose.connect(url, function(err, db) {
+//         if (err) throw err;
+//         //Choosing DB
+//         var dbo = db.db("GoTravel");
+//
+//         //Extracting data from accounts collection
+//         // eslint-disable-next-line no-unused-vars
+//         dbo.collection('Orders').find({}).toArray(function(err, result) {
+//             temp=true
+//             //res.render('OrdersManagementPage',{temp: true})
+//             if (err) throw err;
+//             db.close();
+//         });
+//     });
+//
+// });
+
+
+app.post("/dani", (req, res) => {
+    const url = "mongodb+srv://our-user28:12GoTravel34@cluster0.ofal3.mongodb.net/usersDB?retryWrites=true&w=majority";
+    var mongoose = require('mongodb').MongoClient;
+    mongoose.connect(url, function (err, db) {
         if (err) throw err;
-        console.log(result);
-        db.close();
+        //Choosing DB
+        var dbo = db.db("GoTravel");
+
+        //Extracting data from accounts collection
+        // eslint-disable-next-line no-unused-vars
+        dbo.collection('Orders').find({}).toArray(function (err, result) {
+            //
+            return res.status(200).json({
+                ok: true,
+                data: result
+            });
+            if (err) throw err;
+            db.close();
+        });
     });
 });
+
+app.listen(app_port, () => {
+    console.log('app is runninng. port: '+app_port);
+    console.log('http://127.0.0.1:'+app_port+"/");
+});
+
